fcad245aa53c362dc0db4692fb1a0516af77aeb6
HossamZiad <ziad.h.ramdan@gmail.com>
2022-07-01 17:46:00 +0300

    New Project

---
+++ 05-Multiply-Numbers/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void){
+	int n1;
+	int n2;
+	int total;
+	printf("Enter The First Number: ");
+	scanf("%d",&n1);
+	printf("Enter The Second Number: ");
+	scanf("%d",&n2);
+	total = n1 * n2;
+	printf("Your Total Is: %d",total);
+}
